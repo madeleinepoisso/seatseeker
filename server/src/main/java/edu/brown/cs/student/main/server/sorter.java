7ed523e963c7cc2a2c069ab8d91d513ed06cb09f
Madeleinepoissonnier <madeleine_poissonnier@brown.edu>
2024-05-07 17:27:34 -0400

    feat: radius

--- server/src/main/java/edu/brown/cs/student/main/server/sorter.java
+++ server/src/main/java/edu/brown/cs/student/main/server/sorter.java
@@ -1,9 +1,9 @@
 package edu.brown.cs.student.main.server;
 
-
+import java.lang.Math.*;
 import edu.brown.cs.student.main.server.ticket;
 import java.util.ArrayList;
-import java.util.List
+import java.util.List;
 
 public class EventSorter {
 
@@ -20,7 +20,51 @@ public class EventSorter {
         }
     }
 
-    public static ArrayList bucketSort(float[] arr) {
+    public static boolean withinRange(String city, String searchCity) {
+        api_url1 = 'https://api.api-ninjas.com/v1/geocoding?city={}'.format(city);
+        api_url2 = 'https://api.api-ninjas.com/v1/geocoding?city={}'.format(searchcity);
+        response1 = requests.get(api_url1 + city, headers={'JClbpgBLBGqlkM2vQerDbQ==E64E5q17qvCeNtCU'});
+        response2 = requests.get(api_url2 + searchCity, headers={'JClbpgBLBGqlkM2vQerDbQ==E64E5q17qvCeNtCU'});
+        if (response1.status_code == requests.codes.ok && response2.status_code == requests.codes.ok){
+            city_data = response1.json();
+            search_city_data = response2.json();
+
+            int city_lat = city_data[0]['latitude'];
+            int city_lon = city_data[0]['longitude'];
+            int search_city_lat = search_city_data[0]['latitude'];
+            int search_city_lon = search_city_data[0]['longitude'];
+
+            int R = 6371.0; 
+
+            int lat1 = city_lat * Math.PI / 180;
+            int lon1 = city_lon * Math.PI / 180;
+            int lat2 = search_city_lat * Math.PI / 180;
+            int lon2 = search_city_lon * Math.PI / 180;
+
+            dlon = lon2 - lon1
+            dlat = lat2 - lat1
+
+            a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+            c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+            distance_km = R * c
+            distance_miles = distance_km * 0.621371
+
+            if (distance_miles <= 50){
+                return True
+            }
+            else{
+                return False
+            }
+        }
+    else{
+        //
+        print("Error:", response1.status_code, response1.text);
+        print("Error:", response2.status_code, response2.text);
+    }
+
+
+    public static ArrayList bucketSort(float[] arr, String date, String time, String city) {
         int n = arr.length;
 
         List<Float>[] buckets = new ArrayList[4];
@@ -30,13 +74,13 @@ public class EventSorter {
 
         for (int i = 0; i < n; i++) {
             int val = 0
-            if (arr[i].data) {
+            if (arr[i].date == date ) {
                 val++;
             }
-            if (arr[i].time) {
+            if (arr[i].time == time) {
                 val++;
             }
-            if (arr[i].city) {
+            if (withinRange(city, arr[i].city)) {
                 val++;
             }
             buckets[val].add(arr[i]);
@@ -55,4 +99,5 @@ public class EventSorter {
         }
         return new ArrayList<>(Arrays.asList(Arrays.copyOfRange(arr, 0, 5)));
     }
+}
 
