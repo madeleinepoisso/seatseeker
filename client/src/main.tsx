bfe9c1bf0347206ea7ffb4b11f5fd95283a23925
manolodalbo <manuel_dal_bo@brown.edu>
2024-05-06 23:13:07 -0400

    pullng simone stuff

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-<<<<<<< HEAD
-import "./style/index.css";
-=======
 import "./styles/index.html";
 import "./styles/home.css";
 import "./styles/style.css";
->>>>>>> 18157537406f2dc500d3d0e8498468fcc0113cac
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
